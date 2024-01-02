c27442ecd10095a43e7f234d3696fbea4a987b6d
Alireza <alireza.a@dextern.com>
2024-01-02 03:56:07 +0300

    reserved properties will submited as reserved

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -3,13 +3,14 @@ import getListingById from "@/app/actions/getListingById";
 import ClientOnly from "@/app/components/ClientOnly";
 import EmptyState from "@/app/components/EmptyState";
 import ListingClient from "./ListingClient";
+import getReservations from "@/app/actions/getReservations";
 
 interface IParams {
   listingId?: string;
 }
 const ListingPage = async ({ params }: { params: IParams }) => {
   const listing = await getListingById(params);
-
+  const reservations = await getReservations(params);
   const currentUser = await getCurrentUser();
   if (!listing) {
     return (
@@ -23,6 +24,7 @@ const ListingPage = async ({ params }: { params: IParams }) => {
     <ClientOnly>
       <ListingClient 
         listing={listing}
+        reservations={reservations}
         currentUser={currentUser}
         />
     </ClientOnly>
