77ead0cf2623cbe6a84cae7a5a699888f937a58e
Alireza <alireza.a@dextern.com>
2024-01-02 04:26:32 +0300

    my trips added to burger menu

--- app/actions/getReservations.ts
+++ app/actions/getReservations.ts
@@ -6,15 +6,17 @@ interface IParams {
   authorId?: string;
 }
 
-export default async function getReservations(params: IParams) {
+export default async function getReservations(
+  params: IParams
+) {
   try {
     const { listingId, userId, authorId } = params;
 
     const query: any = {};
-
+        
     if (listingId) {
       query.listingId = listingId;
-    }
+    };
 
     if (userId) {
       query.userId = userId;
@@ -27,14 +29,15 @@ export default async function getReservations(params: IParams) {
     const reservations = await prisma.reservation.findMany({
       where: query,
       include: {
-        listing: true,
+        listing: true
       },
       orderBy: {
-        createdAt: "desc",
-      },
+        createdAt: 'desc'
+      }
     });
 
-    const safeReservations = reservations.map((reservation) => ({
+    const safeReservations = reservations.map(
+      (reservation) => ({
       ...reservation,
       createdAt: reservation.createdAt.toISOString(),
       startDate: reservation.startDate.toISOString(),
@@ -46,7 +49,7 @@ export default async function getReservations(params: IParams) {
     }));
 
     return safeReservations;
-  } catch (error: any){
+  } catch (error: any) {
     throw new Error(error);
   }
 }
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -58,7 +58,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
             cursor-pointer
           "
         >
-          Hotel gasht your home
+          Airbnb your home
         </div>
         <div 
         onClick={toggleOpen}
@@ -119,7 +119,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                   onClick={() => router.push('/properties')}
                 />
                 <MenuItem 
-                  label="Hotel gasht your home" 
+                  label="Airbnb your home" 
                   onClick={rentModal.onOpen}
                 />
                 <hr />
